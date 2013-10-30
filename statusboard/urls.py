0ee06a2e2d2275bfa93b819ea2314666715b4757
Jonathan Chu <jonathan.chu@me.com>
2013-10-30 01:33:52 -0400

    Add statusboard URLs file.

---
+++ statusboard/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from .views.gauges import traffic
+
+
+urlpatterns = patterns(
+    '',
+
+    # gauges
+    url(r'^gauges.json$', traffic, name='gauges'),
+)
